05c3cd3a7c4049416398e514968fa62579da87bd
iSanyok <supergulman09@gmail.com>
2020-12-15 00:32:08 +0300

    add prefix in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth'])->name('dashboard');
-Route::post('/dashboard/add', [PostController::class, 'create'])->middleware(['auth'])->name('add');
-Route::post('/dashboard/complete/{id}', [PostController::class, 'complete'])->middleware(['auth'])->name('complete');
-Route::put('/dashboard/update/{id}', [PostController::class, 'update'])->middleware(['auth'])->name('update');
-Route::delete('/dashboard/delete/{id}', [PostController::class, 'destroy'])->middleware(['auth'])->name('destroy');
+Route::group(['prefix' => '/dashboard', 'middleware' => ['auth']], function () {
+    Route::get('/', [PostController::class, 'index'])->name('dashboard');
+    Route::post('/add', [PostController::class, 'create'])->name('add');
+    Route::post('/complete/{id}', [PostController::class, 'complete'])->name('complete');
+    Route::put('/update/{id}', [PostController::class, 'update'])->name('update');
+    Route::delete('/delete/{id}', [PostController::class, 'destroy'])->name('destroy');
+});
 
-require __DIR__.'/auth.php';
+//Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth'])->name('dashboard');
+//Route::post('/dashboard/add', [PostController::class, 'create'])->middleware(['auth'])->name('add');
+//Route::post('/dashboard/complete/{id}', [PostController::class, 'complete'])->middleware(['auth'])->name('complete');
+//Route::put('/dashboard/update/{id}', [PostController::class, 'update'])->middleware(['auth'])->name('update');
+//Route::delete('/dashboard/delete/{id}', [PostController::class, 'destroy'])->middleware(['auth'])->name('destroy');
+
+require __DIR__ . '/auth.php';
